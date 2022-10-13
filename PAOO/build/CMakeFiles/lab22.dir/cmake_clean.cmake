2aacec67d4111956c3034d51be0ffafd46969056
MunteanNorbert <80778416+MunteanNorbert@users.noreply.github.com>
2022-10-13 16:45:08 +0300

    Add files via upload

---
+++ PAOO/build/CMakeFiles/lab22.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab22.dir/lab2.cpp.o"
+  "lab22"
+  "lab22.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab22.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
