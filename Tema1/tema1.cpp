bf91d2655b50ff4b4767ad53363ab15f5009522b
MunteanNorbert <80778416+MunteanNorbert@users.noreply.github.com>
2022-11-10 16:30:34 +0200

    Update tema1.cpp

--- Tema1/tema1.cpp
+++ Tema1/tema1.cpp
@@ -15,30 +15,32 @@ class Uncopyable{
 class Persoana : private Uncopyable{ //we keep Persoana objects from being copied
     public:
         string nume1;
-	int varsta1;
+		int varsta1;
 
 	Persoana(string nume,int varsta) //Default Constructor
 	:nume1(nume), //this are initializations
 	varsta1(varsta)
 	{} //the ctor body is empty
 
-        ~Persoana() //Destructor
-	{}
+    ~Persoana() //Destructor
+	{
+		cout<<"This is a distructor!"<<endl;
+	}
 
-        //Persoana(const Persoana& persoana) //copy constructor
-        //{}
+    //Persoana(const Persoana& persoana) //Copy constructor
+    //{}
          
-        //Persoana& persoana = (const Persoana &persoana) // Copy assignment operator
-        //{}
+    //Persoana& persoana = (const Persoana &persoana) // Copy assignment operator
+    //{}
 	
 	string getNume()
 	{
-       		return this->nume1;
+       	return this->nume1;
 	}
 
 	int getVarsta()
 	{
-        	return this->varsta1;
+        return this->varsta1;
 	}
 };
 
@@ -47,7 +49,7 @@ class Persoana : private Uncopyable{ //we keep Persoana objects from being copie
 
 int main()
 {
-	Persoana persoana("Alex", 20);
+	Persoana persoana("Norbert", 22);
 	cout<<persoana.getNume()<<" are "<<persoana.getVarsta()<<" ani!"<<endl;
 
 	//Persoana parsoana2(parsoana); //does not compile because of Uncopyable
